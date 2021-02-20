1a77d83259fb0758e629ea33a7a64116d924bfe2
Daniel <itzdanielmichael@gmail.com>
2021-02-20 13:12:11 -0500

    update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,12 +5,13 @@ int main()
 {
   int difficulty = 2;
   int maxDifficulty = 10;
-  while (difficulty <= maxDifficulty){
+  while (difficulty <= maxDifficulty) {
     // playGameAtDifficulty(difficulty)
     std::cin.clear(); // clears the failbit
     std::cin.ignore(); // discards the buffer
-    ++difficulty
+    ++difficulty;
   }
+
   std::cout << "WOW - You're a master hacker!\n";
   return 0; // exit with no error code.
 }
