3d903d546d695a32a9c2cb2606ecac740ed29442
Daniel <itzdanielmichael@gmail.com>
2021-03-05 14:45:21 -0500

    upd

--- tripleX.cpp
+++ tripleX.cpp
@@ -7,16 +7,27 @@ void PlayGameAtDifficulty(int difficulty) {
 
 int main() 
 {
-  int difficulty = 2;
-  int maxDifficulty = 10;
+  // int difficulty = 2;
+  // int maxDifficulty = 10;
 
-  while (difficulty <= maxDifficulty) {
-    PlayGameAtDifficulty(difficulty);
-    std::cin.clear();  // clears the failbit
-    std::cin.ignore(); // discards the buffer
-    ++difficulty;
-  }
+  // while (difficulty <= maxDifficulty) {
+  //   PlayGameAtDifficulty(difficulty);
+  //   std::cin.clear();  // clears the failbit
+  //   std::cin.ignore(); // discards the buffer
+  //   ++difficulty;
+  // }
+  std::cout << "You are a secret agent breaking into a secure server room...";
+  std::cout << std::endl;
+  std::cout << "Enter the correct code to continue...";
 
-  std::cout << "WOW - You're a master hacker!\n";
+  int a = 4;
+  int b = 3;
+  int c = 2;
+
+  std::cout << std::endl;
+  std::cout << a + b + c;
+
+
+  // std::cout << "WOW - You're a master hacker!\n";
   return 0; // exit with no error code.
 }
