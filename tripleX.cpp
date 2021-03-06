b4585d1eab313db9d46abefc2b75f8050fca04f6
Daniel <itzdanielmichael@gmail.com>
2021-03-05 19:52:16 -0500

    update cpp

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> // Preprocessor directive
 #include <ctime>
 
 void PlayGameAtDifficulty(int difficulty) {
   return;
 }
 
-int main() 
+int main() // main function
 {
   // int difficulty = 2;
   // int maxDifficulty = 10;
@@ -16,17 +16,24 @@ int main()
   //   std::cin.ignore(); // discards the buffer
   //   ++difficulty;
   // }
-  std::cout << "You are a secret agent breaking into a secure server room...";
-  std::cout << std::endl;
+  std::cout << "You are a secret agent breaking into a secure server room..."; // expression statements
+  std::cout << std::endl; 
+
   std::cout << "Enter the correct code to continue...";
 
-  int a = 4;
-  int b = 3;
-  int c = 2;
+  const int a = 4; // declaration statements
+  const int b = 3;
+  const int c = 2;
 
-  std::cout << std::endl;
-  std::cout << a + b + c;
+  
+  const int sum = a + b + c;
+  const int product = a * b * c;
 
+  std::cout << std::endl;  // expression statements
+
+  std::cout << "There are 3 numbers in the code" << std::endl; // expression statements
+  std::cout << "The codes add-up to: " << sum << std::endl;
+  std::cout << "The codes multiply to give: " << product << std::endl;
 
   // std::cout << "WOW - You're a master hacker!\n";
   return 0; // exit with no error code.
